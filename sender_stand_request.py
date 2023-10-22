6f0540c6955be8abd73949a077d35404942c0cc0
pc123 <Kalini4eva@yandex.ru>
2023-10-22 03:19:37 -0700

    commit

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -8,29 +8,12 @@ def post_new_order(body):
                          json=body)
 
 
-response = post_new_order(data.body)
-
-
-print(response.status_code)
-print(response.json())
-
-
 def get_track_order(track):
     track_order = post_new_order(data.body).json()["track"]
     return track_order
-
-
-print(response.json())
 
 
 def get_order_track(track):
     track = get_track_order(track)
     payload = {'t': track}
     return requests.get(configuration.URL_SERVICE + configuration.GET_ORDERS_TRACK, params=payload)
-
-
-response = get_order_track(data.order_body)
-
-print(response.status_code)
-print(response.json())
-
